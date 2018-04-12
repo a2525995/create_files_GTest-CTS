0e0c497225b71e6f106ffb0fbeb2bdc0f80db22b
root <root@cnbjlx21783.corpusers.net>
2018-04-12 13:40:51 +0800

    add python_xlsx

---
+++ python_xlsx/Load2.py
@@ -0,0 +1,114 @@
+import xlrd
+from openpyxl import load_workbook
+import openpyxl
+import openpyxl.styles as sty
+k=0
+def changexlmd(file1,sheet1_name,file2,sheet2_name):
+    list1 = []
+    wb = load_workbook(file1)
+
+    sheet = wb[sheet1_name]
+
+
+    sheet1 = xlrd.open_workbook(file1).sheet_by_name(sheet1_name)
+    sheet2 = xlrd.open_workbook(file2).sheet_by_name(sheet2_name)
+    for row in range(sheet1.nrows):
+        if sheet1.cell(row,5).value == 'sony' or sheet1.cell(row,5).value =='Sony' or sheet1.cell(row,5).value == 'SONY':
+            if sheet1.cell(row,11).value == 'Dalian':
+                o = sheet1.cell(row, 2).value
+                if type(o) == float:
+                    if o > 9999:
+                        list1.append(int(o))
+                else:
+                    if o != '' and o.isdigit():
+                        list1.append(int(o))
+
+    print(list1)
+    list2 = []
+    for row in range(sheet2.nrows):
+
+        if sheet2.cell(row,2).value == 'sony' or sheet2.cell(row,2).value =='Sony' or sheet2.cell(row,2).value == 'SONY' :
+            k = sheet2.cell(row, 6).value
+            if  type(k) == float:
+                if k > 9999:
+                    list2.append(int(k))
+            else:
+                if k !='' and k.isdigit():
+                    list2.append(int(k))
+
+    list3 = []
+    kk = (list(set(list1).intersection(list2)))
+
+    for i in range(len(kk)):
+        q = kk[i]
+        list3.append(str(q))
+
+    for qq in range(2,sheet.max_row):
+        for j in range(len(list3)):
+            if str(sheet.cell(row=qq, column=3).value)==list3[j]:
+                for ww in range(1,sheet.max_column):
+                    sheet.cell(row=qq, column=ww).fill = sty.PatternFill(fill_type='solid', fgColor="F0F8FF")
+
+    for row in range(sheet1.nrows):
+        if sheet1.cell(row,5).value == 'SoMC' or sheet1.cell(row,5).value =='SOMC' or sheet1.cell(row,5).value == 'SONY':
+            if sheet1.cell(row,11).value == 'Dalian':
+                o = sheet1.cell(row, 2).value
+                if type(o) == float:
+                    if o > 9999:
+                        list1.append(int(o))
+                else:
+                    if o != '' and o.isdigit():
+                        list1.append(int(o))
+
+    print(list1)
+    list2 = []
+    for row in range(sheet2.nrows):
+
+        if sheet2.cell(row,2).value == 'SOMC' or sheet2.cell(row,2).value =='SoMC':
+            k = sheet2.cell(row, 6).value
+            if  type(k) == float:
+                if k > 9999:
+                    list2.append(int(k))
+            else:
+                if k !='' and k.isdigit():
+                    list2.append(int(k))
+
+
+    list3 = []
+    kk = (list(set(list1).intersection(list2)))
+
+    for i in range(len(kk)):
+        q = kk[i]
+        list3.append(str(q))
+    print(list3)
+    for qq in range(2, sheet.max_row):
+        for j in range(len(list3)):
+            o = str(sheet.cell(row=qq, column=20).value)
+            if type(o) == float:
+
+                k = int(o)
+            else:
+                if o != '' and type(o) != 'NoneType':
+                    if x not in o and ' ' not in o:
+                        if '-' in o:
+                            k = int(o.replace("-", ""))
+
+                        else:
+                            k = int(o)
+
+            if k == list3[j]:
+                for ww in range(1, sheet.max_column):
+                    sheet.cell(row=qq, column=ww).fill = sty.PatternFill(fill_type='solid', fgColor="FFB6C1")
+
+    wb.save("/home/CORPUSERS/xp023799/Downloads/ok.xlsx")
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    changexlmd("/home/CORPUSERS/xp023799/Downloads/Beijing_+IOP+_Devices_List.xlsx","IOP Prototypes",
+               "/home/CORPUSERS/xp023799/Downloads/OCA+proyotypes.xlsx","SOMC Accessory List")
+
